6635496b502411e1d210fd8c770ed0298bcf6c6f
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-14 10:34:17 +0200

    Add new email callback and typing info

--- email_poller.py
+++ email_poller.py
@@ -3,6 +3,8 @@
 
 import argparse
 import time
+from typing import Any
+from typing import Callable
 from typing import Dict
 from typing import List
 from typing import Tuple
@@ -13,8 +15,8 @@ from imaplib import IMAP4_SSL
 from imaplib import IMAP4_SSL_PORT
 
 
-class EmailPoller():
-    """Email polling interface."""
+class EmailConnectionInfo():
+    """Email connection information wrapper."""
 
     def __init__(
         self,
@@ -22,22 +24,34 @@ class EmailPoller():
         email_pass: str,
         email_host: str,
         email_port: int = IMAP4_SSL_PORT,
+    ) -> None:
+        self.user = email_user
+        self.passw = email_pass
+        self.host = email_host
+        self.port = email_port
+
+
+class EmailPoller():
+    """Email polling interface."""
+
+    def __init__(
+        self,
+        email_info: EmailConnectionInfo,
+        callback: Callable[[List[Any]], None],
         search_args: Tuple[Union[str, None], str] = (None, 'ALL'),
     ) -> None:
-        self._email_user = email_user
-        self._email_pass = email_pass
-        self._email_host = email_host
-        self._email_port = email_port
+        self._info = email_info
+        self._callback = callback
         self._search_args = search_args
 
     def _get_server(self) -> IMAP4_SSL:
         """Login and return server object."""
-        server = IMAP4_SSL(self._email_host, self._email_port)
-        result, _ = server.login(self._email_user, self._email_pass)
+        server = IMAP4_SSL(self._info.host, self._info.port)
+        result, _ = server.login(self._info.user, self._info.passw)
         assert result == 'OK', 'login failed!'
         return server
 
-    def _get_email_uids(self) -> Tuple[List[int], Dict]:
+    def _get_email_uids(self) -> List[bytes]:
         """Get all email uids."""
         server = self._get_server()
         server.select('"[Gmail]/All Mail"')
@@ -48,12 +62,12 @@ class EmailPoller():
         server.logout()
         return ids
 
-    def _get_latest_uid(self):
+    def _get_latest_uid(self) -> bytes:
         """Get latest email uid."""
         ids = self._get_email_uids()
         return max(ids)
 
-    def _get_email_from_uid(self, uid: int):
+    def _get_email_from_uid(self, uid: bytes) -> List[Any]:
         """Get an email corresponding to a uid."""
         server = self._get_server()
         server.select('"[Gmail]/All Mail"')
@@ -62,9 +76,8 @@ class EmailPoller():
         server.logout()
         return data
 
-    def _process_new_email(self, raw_data):
-        # TODO
-        print(f'new email! --> \n{raw_data}')
+    def _process_new_email(self, raw_email: List[Any]) -> None:
+        self._callback(raw_email)
 
     def poll(self, period_s: int = 5) -> None:
         """
@@ -110,13 +123,21 @@ def parse_args():
     return parser.parse_args()
 
 
+def test_callback(raw_email: List[Any]) -> None:
+    print(f'new email! --> \n{raw_email}')
+
+
 def main():
     args = parse_args()
-    poller = EmailPoller(
+    info = EmailConnectionInfo(
         args.email_user,
         args.email_pass,
         args.email_host,
         args.email_port,
+    )
+    poller = EmailPoller(
+        info,
+        test_callback,
         ('SUBJECT', 'PATCH'),
     )
     poller.poll()
