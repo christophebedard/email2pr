4ef65925f9cd46916da04d21255e7743924f4a2d
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-14 11:45:14 +0200

    Get patch filename from email data

--- email_to_patch.py
+++ email_to_patch.py
@@ -2,6 +2,43 @@
 
 from email.message import EmailMessage
 import os
+from typing import Tuple
+
+
+def get_patch_index(subject: str) -> Tuple[int, int]:
+    """
+    Get index of patch and the total number of related patches.
+
+    :param subject: the email subject
+    :return: (patch index, total)
+    """
+    # Extract [PATCH ...] substring
+    patch_start = subject.find('PATCH')
+    bracket_open = subject.find('[', 0, patch_start)
+    bracket_close = subject.find(']', bracket_open)
+    patch_string = subject[(bracket_open + 1):bracket_close]
+    # No numbers if there's only one
+    if patch_string == 'PATCH':
+        return 1, 1
+    else:
+        patch_number_sep = patch_string.find('/')
+        index = int(patch_string[patch_number_sep - 1])
+        total = int(patch_string[patch_number_sep + 1])
+        return index, total
+
+
+def get_patch_title(subject: str) -> str:
+    """
+    Get formatted patch title/filename from patch email subject.
+
+    :param subject: the email subject
+    :return: the formatted patch filename
+    """
+    bracket_close = subject.find(']')
+    title = subject[(bracket_close + 1):]
+    title = title.strip()
+    title = title.replace(' ', '-')
+    return title
 
 
 def patch_from_email(msg: EmailMessage, dest_path: str):
@@ -9,7 +46,10 @@ def patch_from_email(msg: EmailMessage, dest_path: str):
     msg_data = msg["date"]
     msg_subject = msg["subject"]
     msg_payload = msg.get_payload()
-    file_name = 'patch.patch'
+
+    index, total = get_patch_index(msg_subject)
+    patch_title = get_patch_title(msg_subject)
+    file_name = f'{index:04}-{patch_title}.patch'
 
     full_path = os.path.join(dest_path, file_name)
     with open(full_path, 'w') as f:
